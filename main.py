7ed8d9e5c695ad04efe641aa8d860239c66eb895
Yuuki-sh <156796005+Yuuki-sh@users.noreply.github.com>
2024-02-05 22:21:35 +0900

    main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ st.title('写真登録用')
 img01 = Image.open('kurumayama.JPG')
 st.image(img01, caption='車山高原', use_column_width=300)
 
-img02 = Image.open('.\pictures\IMG_2085.jpg')
+img02 = Image.open('IMG_2085.jpg')
 st.image(img02, caption='角島', use_column_width=300)
 
-img03 = Image.open('.\pictures\DSC02752--EDIT.jpg')
+img03 = Image.open('DSC02752--EDIT.jpg')
 st.image(img03, caption='灯台', use_column_width=100)
